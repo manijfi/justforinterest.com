d9c98d9253cad139ed8c37ed4c990d14678da302
manijfi <justforinterest.com@gmail.com>
2017-04-18 00:24:44 +0530

    Updated index page
    Index.php page updated with basic under construction message

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <html>
 <head>
-<title>Sample Page</title>
+<link rel="stylesheet" href="style.css">
+<title>JustForInterest</title>
 </head>
 <body>
-<h1> Sample Pange</h1>
+<h1>Just for Interest :) </h1>
+<hr/>
+<div id="body_container">
+&#9758 <i>Site under construction...</i><b>&#9874</b>
+</div>
+<div id="bottom_container">
+<hr/>
+<div id="copyright"> JustForInterest</div>
+</div>
 </body>
 </html>
